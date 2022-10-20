1b8c257bf47fb07e05bb963ca3c9d269877c85f0
fsaiz97 <felixsaiz97@gmail.com>
2022-10-20 11:36:27 +0100

    Update Brewery class Simplify Brewery constructor

--- models/BreweryModel.js
+++ models/BreweryModel.js
@@ -1,53 +1,35 @@
 const breweriesData = require('../breweryDB');
 
 class Brewery {
-  constructor(
-    id,
-    name,
-    brewery_type,
-    street,
-    address_2,
-    address_3,
-    city,
-    state,
-    county_province,
-    postal_code,
-    country,
-    longitude,
-    latitude,
-    phone,
-    website_url,
-    updated_at,
-    created_at
-  ) {
-    this.id = id;
-    this.name = name;
-    this.brewery_type = brewery_type;
-    this.street = street;
-    this.address_2 = address_2;
-    this.address_3 = address_3;
-    this.city = city;
-    this.state = state;
-    this.county_province = county_province;
-    this.postal_code = postal_code;
-    this.country = country;
-    this.longitude = longitude;
-    this.latitude = latitude;
-    this.phone = phone;
-    this.website_url = website_url;
-    this.updated_at = updated_at;
-    this.created_at = created_at;
+  constructor(breweryData) {
+    this.id = breweryData.id;
+    this.name = breweryData.name;
+    this.brewery_type = breweryData.brewery_type;
+    this.street = breweryData.street;
+    this.address_2 = breweryData.address_2;
+    this.address_3 = breweryData.address_3;
+    this.city = breweryData.city;
+    this.state = breweryData.state;
+    this.county_province = breweryData.county_province;
+    this.postal_code = breweryData.postal_code;
+    this.country = breweryData.country;
+    this.longitude = breweryData.longitude;
+    this.latitude = breweryData.latitude;
+    this.phone = breweryData.phone;
+    this.website_url = breweryData.website_url;
+    this.updated_at = breweryData.updated_at;
+    this.created_at = breweryData.created_at;
   }
 
   static showAll() {
     return breweriesData.map(
-      (brewery) => new Brewery(...Object.values(brewery))
+      (brewery) => new Brewery(brewery)
     );
   }
 
   static show(id) {
     if (typeof id === 'string' && id.length > 0) {
-        return new Brewery(...Object.values(breweriesData.find(brewery => brewery.id === id)));
+        return new Brewery(breweriesData.find(brewery => brewery.id === id));
     } else {
         throw {
             statusCode: 404,
@@ -58,7 +40,7 @@ class Brewery {
 
   static showRandom() {
     const randIdx = Math.floor(Math.random() * breweriesData.length);
-    return new Brewery(...Object.values(breweriesData[randIdx]));
+    return new Brewery(breweriesData[randIdx]);
   }
 }
 
